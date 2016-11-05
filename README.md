8c6d529c604b6667dc7b81d842d9e00b149fca19
lee2sman <leetusman@gmail.com>
2016-11-05 02:32:03 -0400

    initial commit of Pon The Road

---
+++ README.md
@@ -0,0 +1,8 @@
+Pon The Road
+============
+
+Pon The Road is a generated novel (based on simple markov generation library Markovify) by *Jack Khrouac*. 
+
+Pon The Road was generated for [NaNoGenMo 2016](https://github.com/NaNoGenMo/2016).
+
+Enjoy.
---
+++ pon-the-road.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Pon The Road Markov generator
+# Lee2sman 2016 cc0
+# requires markovify. pip install markovify
+
+import markovify
+import random
+
+# Get raw text as string.
+with open("source.txt") as f:
+    text = f.read()
+
+# Build the model.
+text_model = markovify.Text(text)
+
+for i in range(2500):
+    # Print five randomly-generated sentences
+    for i in range(random.randint(3,10)):
+        print(text_model.make_sentence())
+    # Print three randomly-generated sentences of no more than 140 characters
+    for i in range(random.randint(5,10)):
+        print(text_model.make_short_sentence(140))
+    print("")
